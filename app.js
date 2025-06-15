cb8fb37cb5077709bf80aa60a3a4062e1743e19d
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-06-15 15:04:57 +0530

    app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const form = document.querySelector('form');
 const resultDiv = document.querySelector('.result');
-
+/*use api for deep informations*/
 form.addEventListener("submit",(e)=>{
     e.preventDefault();
     getWordInfo(form.elements[0].value);
