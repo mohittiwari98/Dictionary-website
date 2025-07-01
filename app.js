02e085e12b6ee8013ca80ee3408ea1f0e4f26c29
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-07-01 22:15:34 +0530

    app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //javascript file
-//const form and
+//const form and r
 const form = document.querySelector('form');
 const resultDiv = document.querySelector('.result');
 /*use api for deep informations*/
